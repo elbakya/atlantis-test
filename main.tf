634825e346d774fc324804683f2d5c3c967b340a
elbakya <34577196+elbakya@users.noreply.github.com>
2022-12-20 01:31:53 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 
-provider "aws" {
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-}
+provider "aws" {}
 
 
 data "aws_availability_zone" "example" {
