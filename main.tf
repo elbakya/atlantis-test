084b3409cb53146c12f1e0cb97a77c432227bbcd
elbakya <34577196+elbakya@users.noreply.github.com>
2022-12-19 21:03:39 +0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+
+provider "aws" {}
+
+
+data "aws_availability_zone" "example" {
+  name = "eu-central-1a"
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*"]
+  }
+}
+
+//Creating Public EC2 Instance
+resource "aws_instance" "Public_EC2" {
+  ami                         = data.aws_ami.latest_ubuntu.id
+  availability_zone           = data.aws_availability_zone.example.name
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  user_data                   = file("./local-setup.sh")
+
+  tags = {
+    Name = "Ec2-with-atlantis"
+  }
+}
