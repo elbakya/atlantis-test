78ae787f0ea08c7b42aab4a0e92cda2a24dcd561
elbakya <34577196+elbakya@users.noreply.github.com>
2022-12-20 01:30:12 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 
-provider "aws" {}
+provider "aws" {
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
 
 
 data "aws_availability_zone" "example" {
