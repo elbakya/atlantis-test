7b58b3898ab21d551339fe09bb9e68fd1a5eeba4
elbakya <34577196+elbakya@users.noreply.github.com>
2022-12-21 12:09:02 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,19 @@
-
 provider "aws" {
-  region = "eu-central-1a"
-
+  region = "eu-central-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
-
-data "aws_availability_zone" "example" {
-  name = "eu-central-1a"
-}
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
-data "aws_ami" "latest_ubuntu" {
-  owners      = ["137112412989"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*"]
-  }
-}
-
-//Creating Public EC2 Instance
 resource "aws_instance" "Public_EC2" {
-  ami                         = data.aws_ami.latest_ubuntu.id
-  availability_zone           = data.aws_availability_zone.example.name
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  #user_data                   = file("./local-setup.sh")
+    ami = "ami-0a261c0e5f51090b1"
+    availability_zone = "eu-central-1a"
+    instance_type               = "t2.micro"
+    associate_public_ip_address = true
 
-  tags = {
+    tags = {
     Name = "Ec2-with-atlantis"
   }
 }
