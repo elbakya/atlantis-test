951243770dd5b659406c55e988b39e0bc5327c78
elbakya <34577196+elbakya@users.noreply.github.com>
2022-12-21 23:55:07 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*
+
 provider "aws" {
   region = "eu-central-1"
   skip_credentials_validation = true
@@ -7,7 +7,7 @@ provider "aws" {
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
 }
-*/
+/*
 variable "aws_secret_key" {
   type = string
 }
@@ -22,7 +22,7 @@ provider "aws" {
   secret_key = var.aws_secret_key
   
 }
-  
+  */
 resource "aws_instance" "Public_EC2" {
     ami = "ami-0a261c0e5f51090b1"
     availability_zone = "eu-central-1a"
